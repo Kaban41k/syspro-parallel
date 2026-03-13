e083ca6ce7f99b85c97ed28bc9eb1f20ad54cbc7
Kaban41k <pig.anishenko@gmail.com>
2026-03-13 10:51:27 +0700

    add some explanation(oops)

--- hw/1.2/main.java
+++ hw/1.2/main.java
@@ -29,11 +29,27 @@ class B extends Thread {
 
 // Could main thread observe x == 1, r_y == 0, r_z == 0?
 // Yes.
+// 
 // A.1->A.2->A.3->A.4->B.1->B.2->B.3->B.4->A.5->B.5
+// 
+// A.1->A.2->A.3->A.4 x == 0, y == 0
+// B.1->B.2->B.3->B.4 x == 0, y == 0, z == 0 
+// A.5 x == 1, y == 0, z == 0
+// B.5 x == 1, y == 0, z == 0
+// r_y == y, r_z == z always
+// x == 1, r_y == 0, r_z == 0!
 //
+// 
 // Could main thread observe x == 2, r_y == 0, r_z == 1?
 // Yes.
+// 
 // A.1->A.2->A.3->B.1->B.2->B.3->A.4->A.5->B.4->B.5
+//
+// A.1->A.2->A.3 x == 0, y == -1, a_x == 0
+// B.1->B.2->B.3 x == 0, z == -1, b_x == 0
+// A.4->A.5 x == 1, y == 0, z == -1
+// B.4->B.5 x == 2, y == 0, z == 1
+// x == 2, r_y == 0, r_z == 1!
 //
 // Could main thread observe x == 1, r_y == 0, r_z == 1?
 // No.
